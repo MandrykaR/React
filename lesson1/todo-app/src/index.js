398a41764b8575688eab7af447a9f78c15226b54
MandrykaR <mandrykaroman01@gmail.com>
2024-02-14 20:14:39 +0100

    l1-t5

---
+++ lesson1/todo-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './styles.css'
+
+const rootElement = document.querySelector('#root')
+
+const titleApp = <h1 className='title'>Todo List</h1>
+
+const root = createRoot(rootElement)
+root.render(titleApp)
