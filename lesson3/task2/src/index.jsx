ffacba1b9ca89230d2360014254378007e881f12
MandrykaR <mandrykaroman01@gmail.com>
2024-02-25 20:47:13 +0100

    l3-t2

---
+++ lesson3/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import './index.scss'
+import './search.scss'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Search from './Search.jsx'
+
+ReactDOM.render(<Search name='Tom' />, document.getElementById('root'))
