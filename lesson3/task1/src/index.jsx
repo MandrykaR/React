1cfb0f9e066dabee0a5b549a148c8104ec0a45b1
MandrykaR <mandrykaroman01@gmail.com>
2024-02-25 20:40:50 +0100

    l3-t1

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -1,7 +1,8 @@
+import './index.scss'
+import './search.scss'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './search.scss' 
-
 import Search from './Search.jsx'
 
 ReactDOM.render(<Search name='Tom' />, document.getElementById('root'))
