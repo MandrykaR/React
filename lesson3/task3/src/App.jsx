b4d92624d9a93809ccf7b6f5435f78eda1abc216
MandrykaR <mandrykaroman01@gmail.com>
2024-02-25 22:22:27 +0100

    l3-t3

--- lesson3/task3/src/App.jsx
+++ lesson3/task3/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import Comment from './Comment.jsx'
 
-const user = {
+import React from 'react'
+
+const userInfo = {
 	name: 'Tom',
 	avatarUrl:
 		'https://avatars.githubusercontent.com/u/105412372?s=400&u=4cb26424388c0fb490a97a8d3aa1f1dc8a31d668&v=4',
 }
 
-function App() {
-	return <Comment user={user} text='Good job!' date={new Date()} />
+const App = () => {
+	return <Comment user={userInfo} text='Good job!' date={new Date()} />
 }
 
 export default App
