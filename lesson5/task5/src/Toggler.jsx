2ce91477aa47ee94f6eaa255cc611b4f20b34428
MandrykaR <mandrykaroman01@gmail.com>
2024-03-03 16:12:34 +0100

    l5-t5

---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Toggler extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			text: 'On',
+		}
+	}
+
+	onToggleText = () => {
+		this.setState({
+			text: this.state.text === 'On' ? 'Off' : 'On',
+		})
+	}
+
+	render() {
+		return (
+			<div onClick={this.onToggleText} className='toggler'>
+				{this.state.text}
+			</div>
+		)
+	}
+}
+
+export default Toggler
