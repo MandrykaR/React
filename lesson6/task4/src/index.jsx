7a71d8c8af35d082fac062009b6adeb7c2f5ff4c
MandrykaR <mandrykaroman01@gmail.com>
2024-03-08 20:21:43 +0100

    l6-t4

---
+++ lesson6/task4/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Status from './Status.jsx'
+
+import './index.scss'
+
+ReactDOM.render(<Status />, document.getElementById('root'))
