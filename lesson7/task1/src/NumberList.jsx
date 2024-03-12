9e79082357935ab37ab696b42039a4ce41bae04b
MandrykaR <mandrykaroman01@gmail.com>
2024-03-12 14:51:37 +0100

    l7-t1

---
+++ lesson7/task1/src/NumberList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NumberList = ({ numbers }) => {
+	return (
+		<ul>
+			{numbers.map(num => (
+				<li>{num}</li>
+			))}
+		</ul>
+	)
+}
+
+export default NumberList
---
+++ lesson7/task1/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import NumberList from './NumberList.jsx'
+
+import './index.scss'
+
+const numbers = [1, 2, 3, 4, 5]
+
+ReactDOM.render(
+	<NumberList numbers={numbers} />,
+	document.getElementById('root')
+)
