4d43a6ff5db10f65edc8c941a0a816f2165b7676
MandrykaR <mandrykaroman01@gmail.com>
2024-03-12 14:58:11 +0100

    l7-t1

--- lesson7/task1/src/index.jsx
+++ lesson7/task1/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import NumberList from './NumberList.jsx'
+import NumbersList from './NumbersList.jsx'
 
 import './index.scss'
 
 const numbers = [1, 2, 3, 4, 5]
 
 ReactDOM.render(
-	<NumberList numbers={numbers} />,
+	<NumbersList numbers={numbers} />,
 	document.getElementById('root')
 )
