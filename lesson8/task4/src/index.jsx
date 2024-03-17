b8c75c6384ee05e364fba690f2d99c94cb5678b2
MandrykaR <mandrykaroman01@gmail.com>
2024-03-17 18:15:44 +0100

    l8-t4

---
+++ lesson8/task4/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import User from './User.jsx'
+
+import './index.scss'
+
+ReactDOM.render(<User userId='facebook' />, document.getElementById('root'))
