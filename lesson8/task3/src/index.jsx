06c75f48ce8d85fad030e6ec1b2b55b1263316be
MandrykaR <mandrykaroman01@gmail.com>
2024-03-17 17:42:18 +0100

    l8-t4

---
+++ lesson8/task3/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Dimensions from './Dimensions.jsx'
+
+import './index.scss'
+
+ReactDOM.render(<Dimensions />, document.getElementById('root'))
