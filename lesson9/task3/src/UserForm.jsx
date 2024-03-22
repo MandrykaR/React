d854e7a6a6a4e75fb18aed1b119eda1af0402199
MandrykaR <mandrykaroman01@gmail.com>
2024-03-22 19:19:15 +0100

    l9-t3

---
+++ lesson9/task3/src/UserForm.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+class UserForm extends React.Component {
+	constructor(props) {
+		super(props)
+		this.formRef = React.createRef()
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+		const formData = new FormData(this.formRef.current)
+		const userData = {}
+		formData.forEach((value, key) => {
+			userData[key] = value
+		})
+		this.props.onSubmit(userData)
+	}
+
+	render() {
+		return (
+			<form
+				ref={this.formRef}
+				onSubmit={this.handleSubmit}
+				className='login-form'
+			>
+				<h1 className='form-title'>Profile</h1>
+
+				<div className='form-control'>
+					<label className='form-label' htmlFor='name'>
+						Name
+					</label>
+					<input className='form-input' type='text' id='name' name='name' />
+				</div>
+
+				<div className='form-control'>
+					<label className='form-label' htmlFor='student'>
+						Student
+					</label>
+					<input
+						className='form-input'
+						type='checkbox'
+						id='student'
+						name='student'
+					/>
+				</div>
+
+				<div className='form-control'>
+					<label className='form-label' htmlFor='occupation'>
+						Occupation
+					</label>
+					<select className='form-input' name='occupation'>
+						<option value='london'>London</option>
+						<option value='new-york'>New York</option>
+						<option value='coconut'>Sidney</option>
+						<option value='mango'>Berlin</option>
+					</select>
+				</div>
+
+				<div className='form-control'>
+					<label className='form-label' htmlFor='about'>
+						About
+					</label>
+					<textarea className='form-input' name='about'></textarea>
+				</div>
+
+				<button className='submit-button' type='submit'>
+					Submit
+				</button>
+			</form>
+		)
+	}
+}
+
+export default UserForm
