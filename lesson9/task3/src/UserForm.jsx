5300bd7c21a4d6517a2a35717a4a986c766e265c
MandrykaR <mandrykaroman01@gmail.com>
2024-03-25 19:24:43 +0100

    l9-t3

--- lesson9/task3/src/UserForm.jsx
+++ lesson9/task3/src/UserForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 class UserForm extends React.Component {
-	handleSubmit = e => {
+	onSubmit = e => {
 		e.preventDefault()
 
 		const formData = new FormData(this.formRef)
@@ -19,11 +19,7 @@ class UserForm extends React.Component {
 
 	render() {
 		return (
-			<form
-				ref={this.setRef}
-				onSubmit={this.handleSubmit}
-				className='login-form'
-			>
+			<form ref={this.setRef} onSubmit={this.onSubmit} className='login-form'>
 				<h1 className='form-title'>Profile</h1>
 
 				<div className='form-control'>
