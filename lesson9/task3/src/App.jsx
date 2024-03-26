219ffa951feade408066ad172f0064a8532a1a96
MandrykaR <mandrykaroman01@gmail.com>
2024-03-26 19:36:01 +0100

    l9-t3

--- lesson9/task3/src/App.jsx
+++ lesson9/task3/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import UserForm from './UserForm.jsx'
+import UserForm from './UserForm'
 
 class App extends React.Component {
+	handleSubmit = formData => {
+		this.createUser(formData)
+	}
+
 	createUser = userData => {
-		console.log(userData)
+		console.log('User Data:', userData)
 	}
 
 	render() {
-		return <UserForm />
+		return (
+			<div>
+				<UserForm onSubmit={this.handleSubmit} />
+			</div>
+		)
 	}
 }
 
