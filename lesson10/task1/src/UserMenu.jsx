e6ab794f73faed87bc793a6f7fbc2526c202a504
MandrykaR <mandrykaroman01@gmail.com>
2024-04-02 19:56:05 +0200

    l10-t1

---
+++ lesson10/task1/src/UserMenu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const UserMenu = ({ userData }) => {
+	if (!userData) {
+		return null
+	}
+
+	const { name, avatar_url } = userData
+	return (
+		<div class='menu'>
+			<span class='menu__greeting'>{name} </span>
+			<img alt='User Avatar' src={avatar_url} class='menu__avatar' />
+		</div>
+	)
+}
+
+export default UserMenu
---
+++ lesson10/task1/src/UserProfile.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class UserProfile extends React.Component {
+	render() {
+		if (!this.props.userData) {
+			return null
+		}
+		const { name, location, avatar_url } = this.props.userData
+		return (
+			<div class='user'>
+				<img alt='User Avatar' src={avatar_url} class='user__avatar' />
+				<div class='user__info'>
+					<span class='user__name'>{name}</span>
+					<span class='user__location'>{location}</span>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default UserProfile
