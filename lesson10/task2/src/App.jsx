aa469180ede65283fb4526be8485d583be315d7d
MandrykaR <mandrykaroman01@gmail.com>
2024-04-05 13:31:19 +0200

    l10-t2

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -26,7 +26,7 @@ class Page extends React.Component {
 			<div className='page'>
 				<h1 className='title'>{`Hello, ${firstName} ${lastName}`}</h1>
 				<main className='content'>
-					<ShoppingCart userData={this.state.userData} />
+					<ShoppingCart userName={this.state.userData} />
 					<Profile
 						userData={this.state.userData}
 						handleChange={this.handleChange}
--- lesson10/task2/src/ShoppingCart.jsx
+++ lesson10/task2/src/ShoppingCart.jsx
@@ -3,31 +3,35 @@ import ProductsList from './ProductsList.jsx'
 import CartTitle from './CartTitle.jsx'
 
 class ShoppingCart extends React.Component {
-	state = {
-		cartItems: [
-			{
-				id: '1',
-				name: 'iPhone 11',
-				price: 999,
-			},
-			{
-				id: '2',
-				name: 'iPhone 12',
-				price: 1200,
-			},
-			{
-				id: '3',
-				name: 'iPhone 17',
-				price: 3232,
-			},
-		],
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			cartItems: [
+				{
+					id: '1',
+					name: 'iPhone 11',
+					price: 999,
+				},
+				{
+					id: '2',
+					name: 'iPhone 12',
+					price: 1200,
+				},
+				{
+					id: '3',
+					name: 'iPhone 17',
+					price: 3232,
+				},
+			],
+		}
 	}
 
 	render() {
 		const count = this.state.cartItems.length
 		return (
 			<div className='column'>
-				<CartTitle userName={this.props.userData.firstName} count={count} />
+				<CartTitle userName={this.props.userName.firstName} count={count} />
 				<ProductsList cartItems={this.state.cartItems} />
 			</div>
 		)
