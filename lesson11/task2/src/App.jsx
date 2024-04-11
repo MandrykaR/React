a531f24ee294dea42f65d56306458b4f40d38fb5
MandrykaR <mandrykaroman01@gmail.com>
2024-04-11 21:32:28 +0200

    l11-t2

---
+++ lesson11/task2/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Dialog from './Dialog.jsx'
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+	state = {
+		isOpen: false,
+	}
+
+	hideDialog = () => {
+		this.setState({
+			isOpen: false
+		})
+	}
+
+
+	showDialog = () => {
+		this.setState({
+			isOpen: true
+		})
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<button onClick={this.showDialog}  className="btn">Show dialog</button>
+				<Dialog isOpen={this.state.isOpen} onClose={this.hideDialog} title="some text">
+					<p>Some text</p>
+				</Dialog>
+			</div>
+		)
+	}
+}
+
+export default App
