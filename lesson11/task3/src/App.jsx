ffdd01b349717c73ba8aea4f0374eedd4a8aeedb
MandrykaR <mandrykaroman01@gmail.com>
2024-04-14 18:00:58 +0200

    l11-t3

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -2,24 +2,12 @@ import React from 'react'
 import Expand from './Expand.jsx'
 
 class App extends React.Component {
-	state = {
-		isOpen: false,
-	}
 
-	toggleExpand = () => {
-		this.setState(prevState => ({
-			isOpen: !prevState.isOpen,
-		}))
-	}
 
 	render() {
 		return (
 			<div className='app'>
-				<Expand
-					title='Some title'
-					isOpen={this.state.isOpen}
-					toggleExpand={this.toggleExpand}
-				>
+				<Expand title='Some title'>
 					<p>
 						Hooks are a new addition in React 16.8. They let you use state and
 						other React features without writing a class.
--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,19 +1,38 @@
 import React from 'react'
 
-const Expand = ({ title, children, isOpen, toggleExpand }) => {
-	return (
-		<div className='expand border'>
-			<div className='expand__header'>
-				<span className='expand__title'>{title}</span>
-				<button onClick={toggleExpand} className='expand__toggle-btn'>
-					<i
-						className={`fa-solid ${isOpen ? 'fa-arrow-up' : 'fa-arrow-down'}`}
-					></i>
-				</button>
+class Expand extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			isOpen: false,
+		}
+	}
+
+	toggleExpand = () => {
+		this.setState(prevState => ({
+			isOpen: !prevState.isOpen,
+		}))
+	}
+
+	render() {
+		const { title, children } = this.props
+
+		return (
+			<div className='expand border'>
+				<div className='expand__header'>
+					<span className='expand__title'>{title}</span>
+					<button onClick={this.toggleExpand} className='expand__toggle-btn'>
+						<i
+							className={`fa-solid ${
+								this.state.isOpen ? 'fa-arrow-up' : 'fa-arrow-down'
+							}`}
+						></i>
+					</button>
+				</div>
+				{this.state.isOpen && <div className='expand__content'>{children}</div>}
 			</div>
-			{isOpen && <div className='expand__content'>{children}</div>}
-		</div>
-	)
+		)
+	}
 }
 
 export default Expand
