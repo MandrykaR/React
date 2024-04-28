775353ee22ad14cfefbf117be4cdf78e372d5df6
MandrykaR <mandrykaroman01@gmail.com>
2024-04-28 16:42:21 +0200

    l14-t3

---
+++ lesson14/task3/src/index.jsx
@@ -0,0 +1,12 @@
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+import Demo from './Demo.jsx'
+
+import './index.scss'
+
+ReactDOM.render(
+	<StrictMode>
+		<Demo />
+	</StrictMode>,
+	document.getElementById('root')
+)
